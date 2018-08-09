3ab5f22d0a4c403fb5dcce7294d187570bfcb812
gsbrunichaki <gabriel.brunichaki@acad.pucrs.br>
2018-08-09 09:36:38 -0300

    recipes screen

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Pastel de Carne',
+      'Um pastel de carne muito saboroso.',
+      'https://xtudoreceitas.com/wp-content/uploads/Pastel-de-Carne-Temperado.jpeg'
+    ),
+
+    new Recipe(
+      'Arroz com Camarão',
+      'Um arroz muito bom, menos para os alérgicos.',
+      'https://www.receiteria.com.br/wp-content/uploads/receitas-de-arroz-com-camarao-747x482.jpg?x17678'
+    )
+  ];
 
   constructor() { }
 
